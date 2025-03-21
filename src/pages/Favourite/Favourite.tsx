ebbb31c59f0fd32551e287e76bfd60123a7c1c49
MB-Prabhu <prabhukumarmb@gmail.com>
2025-03-21 15:26:52 +0530

    segrecated favourties component

--- src/pages/Favourite/Favourite.tsx
+++ src/pages/Favourite/Favourite.tsx
@@ -5,14 +5,12 @@ import { favouriteItems } from "../../Utils/product";
 import { Button, IconButton } from '@mui/material';
 import { DeleteOutline } from "@mui/icons-material";
 import StarRating from "../../components/StarRating/StarRating";
+import FavouriteSingle from "../../subcomponents/FavouriteSingle/FavouriteSingle";
 
 
 const FavouriteItems: React.FC = () => {
   const [favourites, setFavourites] = useState<FavouriteItem[]>(favouriteItems);
 
-  const handleRemove = (id: number) => {
-    setFavourites(favourites.filter(item => item.id !== id));
-  };
 
   return (
     <main className={styles.container}>
@@ -21,26 +19,28 @@ const FavouriteItems: React.FC = () => {
         <p className={styles.empty}>No favourite items added.</p>
       ) : (
         favourites.map((item) => (
-          <div key={item.id} className={styles.favouriteItem}>
-            <img src={item.productImg} alt={item.productTitle} className={styles.image} />
+          // <div key={item.id} className={styles.favouriteItem}>
+          //   <img src={item.productImg} alt={item.productTitle} className={styles.image} />
 
-            <div className={styles.itemDetails}>
-              <h2>{item.productTitle} watcheswatches watches  watcheswatches watches  watcheswatches watches  watches watches</h2>
-              <p>Price: <span>${item.price.toFixed(2)}</span></p>
-              {/* <p>Rating: ⭐{item.rating}</p> */}
-              <p><StarRating rating={item.rating ?? 0} /></p>
+          //   <div className={styles.itemDetails}>
+          //     <h2>{item.productTitle} watcheswatches watches  watcheswatches watches  watcheswatches watches  watches watches</h2>
+          //     <p>Price: <span>${item.price.toFixed(2)}</span></p>
+          //     {/* <p>Rating: ⭐{item.rating}</p> */}
+          //     <p><StarRating rating={item.rating ?? 0} /></p>
 
-             <div className={`${styles.btncontainer}`}>
-              <Button variant="contained" className={styles.addtoCartBtn} >
-                Add To Cart
-              </Button>
+          //    <div className={`${styles.btncontainer}`}>
+          //     <Button variant="contained" className={styles.addtoCartBtn} >
+          //       Add To Cart
+          //     </Button>
 
-              <IconButton  className={styles.deleteBtn} onClick={() => handleRemove(item.id)}>
-                    <DeleteOutline />
-                </IconButton>
-             </div>
-            </div>
-          </div>
+          //     <IconButton  className={styles.deleteBtn} onClick={() => handleRemove(item.id)}>
+          //           <DeleteOutline />
+          //       </IconButton>
+          //    </div>
+          //   </div>
+          // </div>
+
+          <FavouriteSingle item={item} setFavourites={setFavourites}/>
         ))
       )}
     </main>
