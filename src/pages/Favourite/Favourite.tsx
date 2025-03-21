f4ef3173d07d835cf36a01d8cda9f3cde3030f12
MB-Prabhu <prabhukumarmb@gmail.com>
2025-03-21 13:03:11 +0530

    created Favourite page

---
+++ src/pages/Favourite/Favourite.tsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import styles from "./Favourite.module.css";
+import { FavouriteItem } from "../../Types/types";
+import { favouriteItems } from "../../Utils/product";
+import { Button, IconButton } from '@mui/material';
+import { DeleteOutline } from "@mui/icons-material";
+import StarRating from "../../components/StarRating/StarRating";
+
+
+const FavouriteItems: React.FC = () => {
+  const [favourites, setFavourites] = useState<FavouriteItem[]>(favouriteItems);
+
+  const handleRemove = (id: number) => {
+    setFavourites(favourites.filter(item => item.id !== id));
+  };
+
+  return (
+    <main className={styles.container}>
+      <h1 className={styles.heading}>Favourite Items</h1>
+      {favourites.length === 0 ? (
+        <p className={styles.empty}>No favourite items added.</p>
+      ) : (
+        favourites.map((item) => (
+          <div key={item.id} className={styles.favouriteItem}>
+            <img src={item.productImg} alt={item.productTitle} className={styles.image} />
+
+            <div className={styles.itemDetails}>
+              <h2>{item.productTitle} watcheswatches watches  watcheswatches watches  watcheswatches watches  watches watches</h2>
+              <p>Price: <span>${item.price.toFixed(2)}</span></p>
+              {/* <p>Rating: ⭐{item.rating}</p> */}
+              <p><StarRating rating={item.rating ?? 0} /></p>
+
+             <div className={`${styles.btncontainer}`}>
+              <Button variant="contained" className={styles.addtoCartBtn} >
+                Add To Cart
+              </Button>
+
+              <IconButton  className={styles.deleteBtn} onClick={() => handleRemove(item.id)}>
+                    <DeleteOutline />
+                </IconButton>
+             </div>
+            </div>
+          </div>
+        ))
+      )}
+    </main>
+  );
+};
+
+export default FavouriteItems;
